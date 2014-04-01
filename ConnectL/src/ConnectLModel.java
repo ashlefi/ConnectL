de3a20e5f8cff54f5fdb73c0dcfe629b5f6ff416
ashlefi <afish@bgsu.edu>
2014-04-01 10:19:37 -0400

    looks prettier
    moved the brackets around

--- ConnectL/src/ConnectLModel.java
+++ ConnectL/src/ConnectLModel.java
@@ -3,7 +3,8 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-public class ConnectLModel extends JApplet implements MouseListener {
+public class ConnectLModel extends JApplet implements MouseListener
+{
 
 	/**
 	 * 
@@ -15,13 +16,12 @@ public class ConnectLModel extends JApplet implements MouseListener {
 	ConnectLGame game;
 	int w, h;
 
-	private class Canvas extends JPanel {
-		/**
-		 * 
-		 */
+	private class Canvas extends JPanel 
+	{
 		private static final long serialVersionUID = 1L;
 
-		public void paintComponent(Graphics g) {
+		public void paintComponent(Graphics g) 
+		{
 			super.paintComponent(g);
 
 			w = getWidth();
@@ -30,11 +30,13 @@ public class ConnectLModel extends JApplet implements MouseListener {
 			g.setColor( Color.BLUE);
 			g.drawLine(10,10,200,200);
 
-			sqWid = w/cols;
-			sqHgt = h/rows;
+			int sqWid = w/cols;
+			int sqHgt = h/rows;
 
-			for (int i=0; i < rows; i++) {
-				for (int j = 0; j < cols; j++) {
+			for (int i=0; i < rows; i++) 
+			{
+				for (int j = 0; j < cols; j++) 
+				{
 					if ( (i+j)%2 == 0)  g.setColor(Color.blue);
 					else g.setColor(Color.red);
 					g.fillRect(j*sqWid, i*sqHgt, sqWid, sqHgt);
@@ -43,11 +45,11 @@ public class ConnectLModel extends JApplet implements MouseListener {
 
 			for (int r=0; r< 3; r++) {
 				for ( int c = 0; c< 3; c++) {
-					if ( game.getMarkIn(r, c) == tttGame.MARK_O) {
+					if ( game.getValueinLoc(r, c) == game.MARK_BLACK) {
 						g.setColor(Color.WHITE);
 						g.fillOval(c*w/3, r*h/3, w/3, h/3);
 					}
-					if ( game.getMarkIn(r, c) == tttGame.MARK_X) {
+					if (game.getValueinLoc(r, c) == game.MARK_RED) {
 						g.setColor(Color.BLACK);
 						g.fillOval(c*w/3, r*h/3, w/3, h/3);
 					}
@@ -56,12 +58,14 @@ public class ConnectLModel extends JApplet implements MouseListener {
 		}
 	} // end Canvas class
 
-	private class uiHandler implements ActionListener { // The event
-		// listener.
-		public void actionPerformed(ActionEvent e) {
+	private class uiHandler implements ActionListener 
+	{ // The event// listener.
+		public void actionPerformed(ActionEvent e) 
+		{
 			String aC = e.getActionCommand();
-			if (aC.equals("New Game")) {
-				game = new tttGame();
+			if (aC.equals("New Game")) 
+			{
+				game = new ConnectLGame();
 				updateStatus();
 				canvas.repaint();
 			}
@@ -69,86 +73,78 @@ public class ConnectLModel extends JApplet implements MouseListener {
 		}
 	} // end uiHandler class
 
-public void init() { // initialize the applet
+	public void init()
+	{ 
+		game = new ConnectLGame();
+		createComponents();
+		updateStatus();
+	}
+	void updateStatus()
+	{
+		String status;
+		if ( game.getGameState() == game.GAME_STATE_BLACK_TURN)
+			status = "O's Turn!";
+		else if (game.getGameState() == tttGame.GAME_STATE_X_TURN)
+			status = " X's Turn";
+		else if (game.getGameState() == tttGame.GAME_STATE_O_WON )
+			status = "O is the winner";
+		else if (game.getGameState() == tttGame.GAME_STATE_X_WON )
+			status = "X is the winner"; 
+		else
+			status = "Tie Game";
+		gameStatus.setText(status);
+	}
 
-	game = new tttGame();
-	createComponents();
-	updateStatus();
+	public void createComponents() 
+	{
+		JPanel content = new JPanel();
+		content.setLayout(new BorderLayout());
+		this.setSize(400,400);
+		JPanel uip = new JPanel();
+		uip.setLayout(new FlowLayout());
+		gameStatus  = new JTextField(12);
+	
+		JButton b1 = new JButton("New Game");
+		uip.add( new JLabel("status"));
+		uip.add(gameStatus);
+		uip.add(b1);
 
-}
-void updateStatus() {
-	String status;
-	if ( game.getGameState() == tttGame.GAME_STATE_O_TURN)
-		status = " O's Turn";
-	else if (game.getGameState() == tttGame.GAME_STATE_X_TURN)
-		status = " X's Turn";
-	else if (game.getGameState() == tttGame.GAME_STATE_O_WON )
-		status = "O is the winner";
-	else if (game.getGameState() == tttGame.GAME_STATE_X_WON )
-		status = "X is the winner"; 
-	else
-		status = "Tie Game";
-	gameStatus.setText(status);
-}
+		b1.addActionListener( new uiHandler());
 
-public void createComponents() {
+		canvas = new Canvas();
+		canvas.setBackground(new Color(210, 180, 140));
+		canvas.addMouseListener(this);
+		content.add(canvas, BorderLayout.CENTER);
+		content.add(uip, BorderLayout.SOUTH);
+		setContentPane(content);
+	}
 
-	JPanel content = new JPanel();
-	content.setLayout(new BorderLayout());
-	this.setSize(400,400);
-	JPanel uip = new JPanel();
-	uip.setLayout(new FlowLayout());
-	gameStatus  = new JTextField(12);
 
-	JButton b1 = new JButton("New Game");
-	uip.add( new JLabel("status"));
-	uip.add(gameStatus);
-	uip.add(b1);
+	public void mouseClicked(MouseEvent arg0)
+	{
+		int x = arg0.getX();
+		int y = arg0.getY();
 
-	b1.addActionListener( new uiHandler());
+		int col = x / (w/3);
+		int row = y / (h/3);
+		game.placeChecker(row, col);
+		updateStatus();
+		canvas.repaint();
+	}	
 
-	canvas = new Canvas();
-	canvas.setBackground(new Color(210, 180, 140));
-	canvas.addMouseListener(this);
-	content.add(canvas, BorderLayout.CENTER);
-	content.add(uip, BorderLayout.SOUTH);
-	setContentPane(content);
-}
 
-@Override
-public void mouseClicked(MouseEvent arg0) {
-	int x = arg0.getX();
-	int y = arg0.getY();
+	public void mouseEntered(MouseEvent arg0) 
+	{}
 
-	int col = x / (w/3);
-	int row = y / (h/3);
-	game.makeMove(row, col);
-	updateStatus();
-	canvas.repaint();
 
-}
+	public void mouseExited(MouseEvent arg0) 
+	{}
 
-@Override
-public void mouseEntered(MouseEvent arg0) {
-	// TODO Auto-generated method stub
+	public void mousePressed(MouseEvent arg0) 
+	{}
 
-}
 
-@Override
-public void mouseExited(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void mousePressed(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
-
-@Override
-public void mouseReleased(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-
-}
+	public void mouseReleased(MouseEvent arg0) 
+	{}
+	}
 }
